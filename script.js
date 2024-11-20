c43dc42272945e1b7fc141e0076477ad19593294
olaemmadayo <olatoye.temidayo@yahoo.com>
2024-11-20 19:13:49 +0100

    the completion

---
+++ script.js
@@ -0,0 +1,28 @@
+let oruko = "joshua"
+console.log(oruko)
+
+let age=20
+console.log(age)
+
+let food = ["rice", "beans", "garri"]
+let num = [1,2,3,4]
+let studentData = {name:"joseph", age:20, class:"jss3"}
+
+
+console.log(food)
+console.log(num)
+
+const numProp = num.length
+console.log(numProp)
+
+
+const addMap = num.map(n => n+2)
+console.log(addMap)
+
+food.push("corn")
+console.log(food)
+
+num.push(5)
+console.log(num)
+
+console.log(studentData["class"])
