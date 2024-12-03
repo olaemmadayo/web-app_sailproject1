cedd793c7982bc09684a0778929817e87bb8df28
olaemmadayo <olatoye.temidayo@yahoo.com>
2024-12-03 22:07:44 +0100

    moile resposiveness update

--- script.js
+++ script.js
@@ -26,3 +26,47 @@ num.push(5)
 console.log(num)
 
 console.log(studentData["class"])
+
+function sum(a, b){
+    return a + b
+}
+const value = sum(5, 9)
+console.log(value)
+
+
+function subtract(num1, num2){
+    return num1 - num2
+}
+const result = subtract(25, 7)
+console.log(result)
+ 
+function SayHi(){
+    console.log("hello")
+}
+SayHi()
+
+function test(a, b){
+    console.log(a + b)
+}
+test(20, 20)
+
+
+let user = "employee"
+if(user === "guest"){
+    console.log("login failed")
+}
+else if(user === "vistor"){
+    console.log("login failed")
+}
+else if(user === "employee"){
+    console.log("login successful")
+}
+else{
+    console.log("try again later")
+}
+
+for(let i=1; i<=21; i++){
+    if(i % 2 !==0){
+        console.log(i);
+    }
+}
